c268d877c2eb23736f6a5e190cf25507479afa07
luborrer <luborrer@f0r5s2.paris.42.school>
2026-09-19 21:05:58 +0200

    progress

--- ft_valid_pairs.c
+++ ft_valid_pairs.c
@@ -1,10 +1,31 @@
 
+int	valid_pairs(int *pair)
+{
+	int	validpairs[5][2];
+
+	validpairs[0][0] = 4;
+	validpairs[0][1] = 1;
+	validpairs[1][0] = 3;
+	validpairs[1][1] = 2;
+	validpairs[2][0] = 3;
+	validpairs[2][1] = 1;
+	validpairs[3][0] = 2;
+	validpairs[3][1] = 2;
+	validpairs[4][0] = 2;
+	validpairs[4][1] = 1;
+}
+
+
+
+
+
 int	**ft_pairscheck(char *str)
 {
 	int	i;
 	int	j;
-	int	pos;
-	int	**pairs;
+	int	pos1;
+	int	pos2;
+	int	**allpairs;
 
 	i = 0;
 	j = 0;
@@ -13,11 +34,17 @@ int	**ft_pairscheck(char *str)
 
 
 	// Step 1: check pairs
-	i = 0;
-	j = 0;
-	pos = i + j;
+	i = 0;// which row/column
+	j = 0;// where in the row/column
+	
+	
+	while (i < 4 && j < 4)
+	{
+		pos1 = i + j;
+		pos2 = i + 4 + j;
 
-
+		str[pos]
+	}
 
 	// How do I check the pairs?? 
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: luborrer <luborrer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/19 18:48:10 by luborrer          #+#    #+#             */
-/*   Updated: 2026/09/19 19:40:33 by luborrer         ###   ########.fr       */
+/*   Updated: 2026/09/19 21:05:40 by luborrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,12 @@ int	main(int argc, char **argv[])
 		return (1);
 
 	// valid parameters
-	if (ft_valid_input(argv[1]) != 0)
+	if (ft_valid_input(argv[1]) != 1)
 		return (1);
 
+	// from string to int array
+	
+	
 	// valid pairs
 	
 }
