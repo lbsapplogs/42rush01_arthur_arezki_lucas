9528f87f5a29ef67cf3044592ca91be452078d64
luborrer <luborrer@f1br4s7.paris.42.school>
2026-09-20 15:14:23 +0200

    refresh

--- backtracking.c
+++ backtracking.c
@@ -1,1 +1,7 @@
-int backtracking_grille(int grille[4][4], int [])
+int backtracking_grille(int grille[4][4], int ligne, int colonne)  // function to fill les grilles
+{
+    int chiffre;
+
+    if (ligne == 4)
+        return ()
+}
